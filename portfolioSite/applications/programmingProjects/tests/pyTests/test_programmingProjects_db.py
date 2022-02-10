ac30733bdf2bc7cd45d689cbc53e0f1ae5cf7ce9
John Ericson <j.r.ericson@gmail.com>
2022-02-09 16:09:07 -0800

    started adding tests to model

--- portfolioSite/applications/programmingProjects/tests/pyTests/test_programmingProjects_db.py
+++ portfolioSite/applications/programmingProjects/tests/pyTests/test_programmingProjects_db.py
@@ -3,51 +3,92 @@ from django.db import IntegrityError
 
 from portfolioSite.applications.programmingProjects import models
 
+## params to be used in tests
+params_categories = [
+    ("LargeProject"),
+    ("scripting"),
+    ("website"),
+]
 
-def test_dumb():
-    assert 5 == 5
-
-
-def test_dumb2():
-    assert 5 == 6
-
-
-def create_project_categories():
-    large = models.ProjectCategory(category_name="LargeProject")
-    scripting = models.ProjectCategory(category_name="scripting")
-    website = models.ProjectCategory(category_name="website")
-
-# @pytest.mark.dbfixture
-# @pytest.mark.parametrize(
-#     "id, name, slug, is_active",
-#     [
-#         (1, "fashion", "fashion", 1),
-#         (18, "trainers", "trainers", 1),
-#         (35, "baseball", "baseball", 1),
-#     ],
-# )
-# def test_inventory_category_dbfixture(db, db_fixture_setup, id, name, slug, is_active):
-#     result = models.Category.objects.get(id=id)
-#     assert result.name == name
-#     assert result.slug == slug
-#     assert result.is_active == is_active
+## Fixtures
+#########################################################################
 
 
-## create categories
+@pytest.fixture
+def create_project_categories(django_db_blocker):
+    with django_db_blocker.unblock():
+
+        large = models.ProjectCategory(category_name="LargeProject")
+        scripting = models.ProjectCategory(category_name="scripting")
+        website = models.ProjectCategory(category_name="website")
+        large.save()
+        scripting.save()
+        website.save()
+
+
+@pytest.fixture
+def create_projects():
+    # large = models.Project()
+    # scripting = models.ProjectCategory()
+    # website = models.ProjectCategory()
+    # large.save()
+    # scripting.save()
+    # website.save()
+    pass
+
+
+#######################################################
+## tests
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "cat_name",
+    params_categories,
+)
+def test_programmingProjects_catagories_in_db(
+    cat_name,
+    create_project_categories,
+):
+
+    result = models.ProjectCategory.objects.get(category_name=cat_name)
+    assert result.category_name == cat_name
 
 
 ## create new posts with all fields
 
-# search for post in category
+
+def test_programmingProjects_project_in_db(create_project_categories, create_projects):
+    # assert projects have been created
+    # assert that they have correct category
+
+    assert 4 == 4
 
 
-##edit post categoy and tags
-##check that changes were mae
+@pytest.mark.parametrize(
+    "cat_name",
+    params_categories,
+)
+@pytest.mark.django_db
+def test_programmingProjects_delete_category(cat_name, create_project_categories):
+    cat_count = models.ProjectCategory.objects.all().count()
+    assert cat_count == 3
+    item = models.ProjectCategory.objects.get(category_name=cat_name)
+    item.delete()
+    cat_count = models.ProjectCategory.objects.all().count()
+    assert cat_count == 2
 
-##check that post has been changed
+    # TODO - make sure projects still exist if project is deleted and project no longer has category assigned
 
-# delete posts
 
-# check that posts have been deleted
+#     # edit two projects
+
+#     # test that they have been changed in db
+
+#     pass
+
 
 # search by tags
+
+
+# search for post in category
